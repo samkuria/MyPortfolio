3d202cab1ddae9e23b4507c316234bfccf477568
Samuel_Kuria <145743807+samkuria@users.noreply.github.com>
2026-04-30 13:00:10 +0300

    Add Accept header to fetch request

--- js/main.js
+++ js/main.js
@@ -15,7 +15,16 @@
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email, message }),
-          
+        headers:{
+            'Accept': 'application/json'
+        }
+      }).then(response =>{
+          if(response.ok){
+              alert("Thanks for your message😁");
+          }
+          else{
+              alert("Opps!😢There is a problem. Try Again shortly👌");
+          }
       });
 
       const data = await res.json();
